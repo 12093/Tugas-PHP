a42b667c1054164e1aedc7176afa387597d76077
taufiqyfirdaus <42019761+taufiqyfirdaus@users.noreply.github.com>
2019-04-24 19:35:27 +0700

    Tugas PHP
    Tugas PHP

---
+++ Latihan8.php
@@ -0,0 +1,12 @@
+<?php
+$arrayNilai = array("Ani" => 80, "Otim" => 90, "Ana" => 75, "Budi" => 85);
+echo $arrayNilai ['Ani'];
+echo $arrayNilai ['Otim'];
+
+$arrWarna = array();
+$arrWarna['Ami'] = 80;
+$arrWarna['Asma'] = 95;
+$arrWarna['Sri'] = 77;
+echo $arrWarna['Asma'];
+echo $arrWarna['Ami'];
+ ?>
